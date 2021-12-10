29f911c9cf5e2cca55524e132accd6ae2db709fa
anandthirumala <a.ts@outlook.com>
2021-12-10 15:13:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# table-row-drag-drop-52ebtw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/table-row-drag-drop-52ebtw)
---
+++ app/table-basic-example.ts
@@ -0,0 +1,58 @@
+import {Component, ViewChild} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem, CdkDragHandle} from '@angular/cdk/drag-drop';
+import {MatTable} from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
+/**
+ * @title Basic use of `<table mat-table>`
+ */
+@Component({
+  selector: 'table-basic-example',
+  styleUrls: ['table-basic-example.css'],
+  templateUrl: 'table-basic-example.html',
+})
+export class TableBasicExample {
+  @ViewChild('table') table: MatTable<PeriodicElement>;
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+
+//    drop(event: CdkDragDrop<string[]>) {
+//     moveItemInArray(this.dataSource, event.previousIndex, event.currentIndex);
+//   }
+
+//   onListDrop(event: CdkDragDrop<string[]>) {
+//   // Swap the elements around
+//   moveItemInArray(this.dataSource, event.previousIndex, event.currentIndex);
+// }
+
+dropTable(event: CdkDragDrop<PeriodicElement[]>) {
+    const prevIndex = this.dataSource.findIndex((d) => d === event.item.data);
+    moveItemInArray(this.dataSource, prevIndex, event.currentIndex);
+    this.table.renderRows();
+  }
+
+}
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
